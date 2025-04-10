8973edca007b6b957c563bbcf0231d415069ea69
Bob Oyier <boboyier92@gmail.com>
2025-04-10 10:35:22 +0300

    Introduction to useState hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
+import { useState } from 'react';
 import AddTaskForm from './components/AddTaskForm';
+import Button from './components/Button';
 import Header from './components/Header';
 import TasksContainer from './components/TasksContainer';
 import TasksCount from './components/TasksCount';
 
 const App = () => {
+	const [count, setCount] = useState(200);
+
+	let numb = 11;
+	function directDOMManipulation() {
+		const h2 = document.querySelector('h2');
+		h2.innerText = numb += 1;
+	}
+
+	function stateManipulation() {
+		setCount(count + 1);
+	}
+
 	return (
 		<div className="container">
+			<h2>{numb}</h2>
+			<Button id="add-task-btn" onClick={directDOMManipulation}>
+				Add
+			</Button>
+
+			<h1>{count}</h1>
+
+			<Button id="add-task-btn" onClick={stateManipulation}>
+				Add
+			</Button>
+
+			<Button id="add-task-btn">Subtract</Button>
+
 			<Header />
 			<AddTaskForm />
 			<TasksContainer />
-			<TasksCount/>
+			<TasksCount />
 		</div>
 	);
 };
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -2,6 +2,9 @@ import Button from './Button';
 import Input from './Input';
 
 const TaskItem = ({ task }) => {
+	
+	const { completed, title } = task
+	
 	function handleClick(taskName) {
 		alert(`The clicked task is: ${taskName}`);
 	}
@@ -11,14 +14,14 @@ const TaskItem = ({ task }) => {
 			<Input
 				type="checkbox"
 				className="checkbox"
-				checked={task.completed === true}
+				checked={completed === true}
 			/>
 
-			<span>{task.title}</span>
+			<span>{title}</span>
 
 			<Button
 				buttonClass="delete-button"
-				onClick={() => handleClick(task.title)}
+				onClick={() => handleClick(title)}
 			>
 				<i className="fa-solid fa-trash"></i>
 			</Button>
